3a7b6519e56a82daf4cc505b1e1dd0a0eafcd50a
Marco Giancarli <m.a.giancarli@gmail.com>
2014-04-13 08:45:13 -0400

    proxies n shit not working

--- voter.py
+++ voter.py
@@ -1,11 +1,17 @@
-import requests
 import sys
+import pymongo
+from bs4 import BeautifulSoup
+import requests
+import urllib
 
 if len(sys.argv) != 4:
     print 'Invalid params! Usage: <image key> <number of votes> <up/down>'
     exit(1)
 
-imageUrl = 'https://imgur.com/' + sys.argv[1]
+
+LOGIN_URL = ''
+LOGUT_URL = ''
+IMAGE_URL = 'https://imgur.com/' + sys.argv[1]
 numVotes = int(sys.argv[2])
 
 if sys.argv[3] == 'up':
@@ -16,9 +22,14 @@ else:
     print 'You need to specify "up" or "down" for the third param.'
     exit(1)
 
-proxyDict = {'https':'184.182.240.131:19315'}
-response = requests.get('http://icanhazip.com', proxies=proxyDict)
+# go through list of proxies until valid one is confirmed
+# if no valid proxies attached to account, select 3 new ones
+
+proxies = {'https':'202.118.236.130:3128'}
+
+response = requests.get('http://icanhazip.com',proxies=proxies)
 print response.text
+
 # login
 
 # upvote
