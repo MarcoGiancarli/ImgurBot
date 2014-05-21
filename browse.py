3bc08472c08f2eaeb7438a20a009a1b84664af6b
Marco Giancarli <m.a.giancarli@gmail.com>
2014-05-20 20:17:45 -0400

    browser fixed, everything seems to be working correctly

--- browse.py
+++ browse.py
@@ -9,49 +9,60 @@ db = Database()
 
 for account in db.next_login():
     try:
-        print 'Trying to log in on user',account['username'],'('+account['username']+')...'
+        print 'Trying to log in on user',account['username'],'('+account['password']+')...'
         active_proxy = db.fix_proxies(account)
         bot = Imgurbot(active_proxy)
         bot.login(account['username'], account['password'])
-        bot.nav_to_gallery()
+        bot.nav_to_usersub()
         print 'Logged in as user',account['username']
     except:
         print 'Failed to log in to user',account['username']
 
     # simulate browsing on the user as long as it is still logged in
     login_status = True
-    BASE_PROB = 750
+    fail_count = 0
+    BASE_PROB = 500
     while login_status:
-        action_index = random.randint(0,1000000)
-        if action_index < BASE_PROB:
+        action_index = random.randint(0,400000)
+        if action_index < BASE_PROB or fail_count > 5:
             try:
                 sys.stdout.write('Attempting to log out of account...')
                 sys.stdout.flush()
                 bot.logout()
+                bot.driver.quit()
                 login_status = False
+                print ' SUCCESS'
+                fail_count = 0
             except:
                 print ' FAILED'
+                fail_count += 1
         elif action_index < 6 * BASE_PROB:
             try:
                 sys.stdout.write('Attempting to comment on the current post...')
                 sys.stdout.flush()
-                bot.comment('lolwut') # test comment
+                bot.comment(db.viralize())
                 print ' SUCCESS'
+                fail_count = 0
             except:
                 print ' FAILED'
+                fail_count += 1
         elif action_index < 10 * BASE_PROB:
             try:
                 sys.stdout.write('Attempting to vote on the current post...')
                 sys.stdout.flush()
                 bot.auto_vote()
                 print ' SUCCESS'
+                fail_count = 0
             except:
                 print ' FAILED'
-        elif action_index < 300 * BASE_PROB:
+                fail_count += 1
+        elif action_index < 125 * BASE_PROB:
             try:
                 print 'Next.'
                 bot.go_to_next()
+                fail_count = 0
             except:
                 print 'Could\'nt find the "next" button.'
-        sleepTime = random.uniform(0.5,1.9)
+                fail_count += 1
+        sleepTime = random.uniform(0.9,4.5)
         time.sleep(sleepTime)
--- imgurbot.py
+++ imgurbot.py
@@ -42,6 +42,13 @@ class CantFindElementError(Exception):
         return self.element + ' at page: ' + self.url
 
 
+class NoRepostError(Exception):
+    def __init__(self, post_url):
+        self.post_url = post_url
+    def __str__(self):
+        return 'Couldn\'t find a repost for '+self.post_url
+
+
 # This class will handle most of the automation in selenium.
 class Imgurbot:
     def __init__(self, proxy):
@@ -105,12 +112,13 @@ class Imgurbot:
             # when we see either of these two elements, we have succeeded or failed
             creating_account = True
             while creating_account:
+                result_url = self.driver.current_url
                 # the name will be in the url if you register without error.
-                if username.lower() in self.driver.current_url:
+                if username.lower() in result_url.lower():
                     print 'Captcha entered correctly. Would you like a cookie or a gold star?'
                     correct = True
                     creating_account = False
-                elif self.driver.current_url != REGISTER_URL:
+                elif result_url != REGISTER_URL:
                     print 'You seem to have left the sign up page, or the page is unaccessible.'
                     break # this will force us to redirect to sign up page, which
                           # redirects us to the user profile if signed in.
@@ -148,11 +156,11 @@ class Imgurbot:
             except:
                 print 'Waiting to find logout button...'
             attempts += 1
-            if attempts>10:
+            if attempts>=3:
                 raise CantFindElementError(self.driver.current_url, 'logout') # caused by https failing
 
 
-    def vote(self, url_with_link, link_text, vote_up): # for vote, True is up and False is down.
+    def vote(self, url_with_link, link_text, vote_up): # for vote_up, True is up and False is down.
         self.driver.get(url_with_link)
         try:
             link_to_img = self.wait8s.until(lambda driver:driver.find_element_by_partial_link_text(link_text))
@@ -173,11 +181,14 @@ class Imgurbot:
             raise CantFindElementError(self.driver.current_url, 'arrow')
 
 
-    def nav_to_gallery(self):
-        self.driver.get('https://imgur.com')
+    def nav_to_usersub(self):
+        new_hot = 'hot'
+        if random.randint(1,3) == 1:
+            new_hot = 'new'
+        self.driver.get('https://imgur.com/'+new_hot+'/time')
         try:
-            next_button = self.wait8s.until(lambda driver:driver.find_element_by_class_name('post'))
-            next_button.click()
+            first_post = self.wait8s.until(lambda driver:driver.find_element_by_class_name('post'))
+            first_post.click()
         except:
             raise CantFindElementError(self.driver.current_url, 'post')
 
@@ -192,39 +203,55 @@ class Imgurbot:
 
     def auto_vote(self):
         try:
-            up_counter = self.wait8s.until(lambda driver:driver.find_element_by_class_name('title positive'))
-            down_counter = self.wait8s.until(lambda driver:driver.find_element_by_class_name('title negative'))
-            ups = int(up_counter.get_attribute('original-title'))
-            downs = int(down_counter.get_attribute('original-title'))
-            if random.randint(0,ups+downs) < ups:
-                button_id = 'mainUpArrow'
+            #up_counter = self.wait8s.until(lambda driver:driver.find_element_by_class_name('title positive'))
+            #down_counter = self.wait8s.until(lambda driver:driver.find_element_by_class_name('title negative'))
+            #ups = int(up_counter.get_attribute('original-title'))
+            #downs = int(down_counter.get_attribute('original-title'))
+            #if random.randint(0,ups+downs) < ups:
+            #    button_id = 'mainUpArrow'
+            #else:
+            #    button_id = 'mainDownArrow'
+            body = self.wait8s.until(lambda driver:driver.find_element_by_tag_name('body'))
+            if random.randint(0,20) < 16:
+                body.send_keys('+')
+            elif random.randint(0,1) == 1:
+                body.send_keys('0')
             else:
-                button_id = 'mainDownArrow'
-            vote_button = self.wait8s.until(lambda driver:driver.find_element_by_id(button_id))
+                body.send_keys('-')
             # if the button is already pushed, we don't want to remove that vote!
-            if 'pushed' not in voteButton.get_attribute('class'):
-                vote_button.click()
+            #if 'pushed' not in voteButton.get_attribute('class'):
+            #    vote_button.click()
         except:
-            raise CantFindElementError(self.driver.current_url, 'arrow')
+            raise CantFindElementError(self.driver.current_url, 'body')
 
 
     def comment(self, comment_text):
         try:
-            comment_button = self.wait8s.until(lambda driver:driver.find_element_by_id('submit-caption-button'))
             comment_field = self.wait8s.until(lambda driver:driver.find_element_by_id('caption_textarea'))
             comment_field.click()
-            comment_field.send_keys(comment_text)
-            comment_button.click()
+            comment_field.send_keys(comment_text + '\n')
         except:
-            raise CantFindElementError(self.driver.current_url, 'commentbox')
+            raise CantFindElementError(self.driver.current_url, 'comment_field')
 
 
-    def get_comment_text(self):
-        # take caption, image, and url.
-        # look for reposts and take a random good comment from there
-        # if nothing found, then search for comments on page or use a viralizer comment
-        comment_text = 'fag'
-        return comment_text
+#    def get_comment_text(self,db):
+#        try:
+#            post_url = self.driver.current_url
+#            image_location = # extract image location from driver
+#            self.driver.get('http://images.google.com')
+#            query_field = self.wait8s.until(lambda driver:driver.find_element_by_id('lst-ib'))
+#            # input image location to query field
+#            # click search
+#            # do something with results
+#            if theres an imgur post where the url isnt the post_url,
+#                # scrape that post for one of the top 3 comments
+#            else
+#                raise NoRepostError(post_url)
+#            # return to original post
+#            self.driver.get(post_url)
+#        except NoRepostError:
+#            comment_text = db.viralizer
+#        return comment_text
 
 
 
@@ -253,6 +280,9 @@ class Database:
         with open('nouns.txt') as f:
             self.nouns = f.read().splitlines()
         self.NOUNS_SIZE = len(self.nouns)
+        with open('comments.txt') as f:
+            self.viralizer = f.read().splitlines()
+        self.COMMENTS_SIZE = len(self.viralizer)
 
 
     def shuffle_logins(self):
@@ -377,6 +407,12 @@ class Database:
             indices.append(randomIndex)
             size -= 1
         return indices
+
+
+    def viralize(self):
+        random_index = random.randint(0,self.COMMENTS_SIZE)
+        comment = self.viralizer[random_index]
+        return comment
 
 
     def test_proxy(self,proxy):
